12477820337849880d94c30bcf5eb486f09c836b
Matthew Androus <matthewandrous@gmail.com>
2021-06-02 13:26:53 -0700

    First commit with basic React project

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = firebase.initializeApp({
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectID: '',
+    storageBucket: '',
+    messageSenderId: '',
+    apiId: '',
+});
+
+export {firebseConfig as firebase };
