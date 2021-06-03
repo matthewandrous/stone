9d63d32e1fac5f629368401eee37c34ecfd1ac10
Matthew Androus <matthewandrous@gmail.com>
2021-06-02 18:25:17 -0700

    added integration with Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
 const firebaseConfig = firebase.initializeApp({
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectID: '',
-    storageBucket: '',
-    messageSenderId: '',
-    apiId: '',
+    apiKey: "AIzaSyDD7jdAfe3RqDnrE78Xfmau0F89g08UXQg",
+        authDomain: "stone-676ec.firebaseapp.com",
+        projectId: "stone-676ec",
+        storageBucket: "stone-676ec.appspot.com",
+        messagingSenderId: "224157942796",
+        appId: "1:224157942796:web:385baa1e1968a17069da29",
+        measurementId: "G-79NMVVXBWZ",
 });
 
-export {firebseConfig as firebase };
+export {firebaseConfig as firebase };
